8e3eb148116fcc8906ea4b2d63ac0f464d5f0557
DIKA-MD <108252774+DIKA-MD@users.noreply.github.com>
2022-07-26 04:32:18 +0700

    Hshdh.js
    Heb

--- settings.js
+++ settings.js
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Cylen-MD" //namabot kalian
+global.ownername= "ᴹᴿ᭄ DikaaOfcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.dikaofc.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/Cylen-MD" //bebas asal jan hapus
+global.github = "https://DIKA-MD.github.io/" //bebas
+global.email = "nainnadika6@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6288223674569'] //premium user
+global.owner = ["6288223674569","6283142211902"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6288223674569" // nomor wa kalian
+global.ownernomerr = "+6288223674569" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Cylen-MD' //sticker wm ubah
+global.author = 'Di Buat Oleh DikaOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
